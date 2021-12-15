865d45e9e0b1073e1aefe6408c701486a218820d
SerhiiLytviak <litvyak.sergey@gmail.com>
2021-12-15 16:20:21 +0200

    Сделано 7-е ДЗ
    Выполнены обе задачи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkUp = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkUp);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+  `;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  const isGalleryImage = event.target.classList.contains("gallery__image");
+
+  if (!isGalleryImage) {
+    return;
+  }
+
+  basicLightbox
+    .create(
+      `
+  	<img width="1400" height="900" src="${event.target.dataset.source}">
+  `
+    )
+    .show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkUp = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkUp);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  `;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  const isGalleryImage = event.target.classList.contains("gallery__image");
+
+  if (!isGalleryImage) {
+    return;
+  }
+}
